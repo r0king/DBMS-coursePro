c65ca9a49a96d12ef9829737750e0615ecbbc7ac
kng <kng@pop-os.localdomain>
2022-02-23 21:57:35 +0530

    change manytomany

--- examMan/models.py
+++ examMan/models.py
@@ -1,5 +1,4 @@
 
-from pyexpat import model
 from django.db import models
 
 class CourseOffering(models.Model):
@@ -12,19 +11,18 @@ class CourseOffering(models.Model):
         return self.course_code
 
 
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    program = models.CharField(max_length=150)
+    course = models.ForeignKey(CourseOffering,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
 class Exam(models.Model):
     name = models.CharField(max_length=50)
     place = models.CharField(max_length=150)
     time = models.DateTimeField('date and time')    
 
-    def __str__(self):
-        return self.name
-
-class Student(models.Model):
-    name = models.CharField(max_length=50)
-    program = models.CharField(max_length=150)
-    course = models.ForeignKey(CourseOffering,on_delete=models.CASCADE)
-    exams = models.ManyToManyField(Exam)
     def __str__(self):
         return self.name
 
